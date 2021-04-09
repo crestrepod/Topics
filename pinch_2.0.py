226273e02273adedb14ff169272046d451d42b16
crestrepod <82171627+crestrepod@users.noreply.github.com>
2021-04-09 18:07:31 -0500

    Update and rename pinch.py to pinch_2.0.py

---
+++ pinch_2.0.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def fun(Cph,Cpc,Th,Tc):
+    
+    T  = np.array([])
+    for i in range(len(Th)):
+        T = np.append(T, Th[i,:]) 
+    for i in range(len(Tc)):
+        T = np.append(T, Tc[i,:]) 
+        
+    dTh = np.array([])
+    for i in range(len(Th)):
+        dTh = np.append(dTh, Th[i,1]-Th[i,0])
+
+    dTc = np.array([])
+    for i in range(len(Tc)):
+        dTc = np.append(dTc, Tc[i,1]-Tc[i,0])
+
+    Qh = np.array([])
+    for i in range(len(Th)):
+        Qh = np.append(Qh, Cph[i]*dTh[i])
+
+    Qc = np.array([])
+    for i in range(len(Tc)):
+        Qc = np.append(Qc, Cpc[i]*dTc[i])
+
+    Qh_acc = np.array([0])
+    for i in range(len(Qh)):
+        Qh_acc = np.append(Qh_acc, Qh_acc[i]+Qh[i])
+
+    Qc_acc = np.array([0])
+    for i in range(len(Qc)):
+        Qc_acc = np.append(Qc_acc, Qc_acc[i]+Qc[i])
+    return(Qh, Qc, Qh_acc, Qc_acc)

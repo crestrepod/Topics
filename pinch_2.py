85a9463799de02fa69f8b5b10a86aeb8bcef85d8
crestrepod <82171627+crestrepod@users.noreply.github.com>
2021-04-11 14:56:52 -0500

    Update pinch_2.py

--- pinch_2.py
+++ pinch_2.py
@@ -1,23 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 09:03:10 2021
+
+@author: Camilo
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
-def fun(Cph,Cpc,Th,Tc):
-    Cph = np.array(Cph)
-    Cpc = np.array(Cpc)
-    Th  = np.array(Th) 
-    Tc  = np.array(Tc)
-	
+def data(Cph,Cpc,Th,Tc):
+    #Calculo T, dT, Q
     T  = np.array([])
     for i in range(len(Th)):
         T = np.append(T, Th[i,:]) 
     for i in range(len(Tc)):
         T = np.append(T, Tc[i,:]) 
-        
     dTh = np.array([])
     for i in range(len(Th)):
         dTh = np.append(dTh, Th[i,1]-Th[i,0])
-
     dTc = np.array([])
     for i in range(len(Tc)):
         dTc = np.append(dTc, Tc[i,1]-Tc[i,0])
@@ -25,7 +25,6 @@ def fun(Cph,Cpc,Th,Tc):
     Qh = np.array([])
     for i in range(len(Th)):
         Qh = np.append(Qh, Cph[i]*dTh[i])
-
     Qc = np.array([])
     for i in range(len(Tc)):
         Qc = np.append(Qc, Cpc[i]*dTc[i])
@@ -37,25 +36,69 @@ def fun(Cph,Cpc,Th,Tc):
     Qc_acc = np.array([0])
     for i in range(len(Qc)):
         Qc_acc = np.append(Qc_acc, Qc_acc[i]+Qc[i])
-		
-		
-    hot = pd.DataFrame({'T in': Th[:,1],
-                    'T out':Th[:,0],
-                    'Cp':   Cph,
-                    'Q':Qh,
-                    'Q acc':Qh_acc[1:]
-                    })
-  
-    print(hot)
-    cold = pd.DataFrame({'T in': Tc[:,1],
-                    'T out':Tc[:,0],
-                    'Cp':   Cpc,
-                    'Q':Qc,
-                    'Q acc':Qc_acc[1:]
-                    })
-    print(cold)
+    
+#Cálculo Corrientes Compuestas 
+    #Caliente
+    hot = []
+    hot = np.append(hot, (Th[:]))
+    hot.sort()
+
+    Cph_acu = np.array([])
+    result = np.where(Th == hot[0])
+    Cph_acu = np.append(Cph_acu, Cph[int(result[0])])
+
+    for i in range(1, len(hot)):
+        Cpi = 0
+        for n in range(len(Th)):
+            if hot[i] in Th[n] or (hot[i] > Th[n][0] and hot[i] < Th[n][1]): 
+                Cpi = Cpi + Cph[n]
+        if Cpi != 0 : 
+            Cph_acu = np.append(Cph_acu,Cpi)
+
+    #Código raro para eliminar Cps repetidos consecutivos
+    n = np.array([Cph_acu[0]])
+    for i in range(1,len(Cph_acu)):
+       if Cph_acu[i] != Cph_acu[i-1]:
+           n = np.append(n,Cph_acu[i])
+    Cph_acu = n       
+
+
+    #Código Calcular Q Compuesto
+    Qh_comp = np.array([0])       
+
+    for i in range(len(Cph_acu)):
+        Qh_comp = np.append(Qh_comp, Qh_comp[i] + (Cph_acu[i]*(hot[i+1]-hot[i])))
         
-    return(Th, Tc, Cph, Cpc, Qh, Qc, Qh_acc, Qc_acc)
+    #Fria
+    cold = []
+    cold = np.append(cold, (Tc[:]))
+    cold.sort()
+
+    Cpc_acu = np.array([])
+    result = np.where(Tc == cold[0])
+    Cpc_acu = np.append(Cpc_acu, Cpc[int(result[0])])
+    for i in range(1, len(cold)):
+        Cpi = 0
+        for n in range(len(Tc)):
+            if cold[i] in Tc[n] or (cold[i] > Tc[n][0] and cold[i] < Tc[n][1]): 
+                Cpi = Cpi + Cpc[n]
+        if Cpi != 0 : 
+            Cpc_acu = np.append(Cpc_acu,Cpi)
+
+#Código raro para eliminar Cps repetidos consecutivos
+    n = np.array([Cpc_acu[0]])
+    for i in range(1,len(Cpc_acu)):
+        if Cpc_acu[i] != Cpc_acu[i-1]:
+            n = np.append(n,Cpc_acu[i])
+    Cpc_acu = n       
+
+
+#Código Calcular Q Compuesto
+    Qc_comp = np.array([0])       
+    for i in range(len(Cpc_acu)):
+        Qc_comp = np.append(Qc_comp, Qc_comp[i] + (Cpc_acu[i]*(cold[i+1]-cold[i])))
+
+    return(Qh,Qc,Qh_acc,Qc_acc,hot,cold,Qh_comp,Qc_comp)
 
 	
 def plot(Tc):
